aac3738697959f6c18cb99cbf59530975dce300a
shubham32sk <152477521+shubham32sk@users.noreply.github.com>
2024-04-29 20:12:04 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,9 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
+    implementation("com.google.android.gms:play-services-pal:20.2.0")
     testImplementation("junit:junit:4.13.2")
+    implementation("com.google.android.material:material:1.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -4,57 +4,116 @@ import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.PointerIcon
 import android.widget.Button
+import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
+import com.google.ads.interactivemedia.pal.utils.Duration
 import com.google.android.filament.View
+import com.google.android.material.slider.RangeSlider
+import kotlin.time.toDuration
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var music: MediaPlayer
     private var currentIndex = 0
-    //lateinit var seekbar:SeekBar
-    lateinit var vtext:TextView
+    lateinit var seekBar: SeekBar
+    lateinit var vtext: EditText
+    private val song= listOf("Beat_trap","Sitar","Flute")
     private val audiofiles = listOf(R.raw.beat_trap, R.raw.sitar, R.raw.flute)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnstart: Button = findViewById(R.id.btnstart)
-        val btnpause: Button = findViewById(R.id.btnpause)
+        val btnStart: Button = findViewById(R.id.btnstart)
+        val btnPause: Button = findViewById(R.id.btnpause)
         val btnNext: Button = findViewById(R.id.btnNext)
+        seekBar = findViewById(R.id.seekBar)
 
 
-        vtext=findViewById(R.id.song_name)
+        vtext=findViewById(R.id.title)
         music = MediaPlayer()
 
+        //setupseekbar
+        setUpSeekBar()
 
-        btnstart.setOnClickListener {
+
+
+        btnStart.setOnClickListener {
             musicstart()
+
         }
-        btnpause.setOnClickListener { musicpause() }
+        btnPause.setOnClickListener { musicpause() }
         btnNext.setOnClickListener { nextmusic() }
+
+
+
+
+
     }
+
+    private fun setdata() {
+        vtext.setText(song[currentIndex])
+
+    }
+
+    private fun setUpSeekBar() {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser){
+                    music?.seekTo(progress)}
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                music?.pause()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                music?.start()
+            }
+        }) }
+    private val seekBarUpdateHandler=Handler()
+    private val seekBarUpdateRunnable=object :Runnable{
+        override fun run() {
+            music?.let {
+                seekBar.progress = it.currentPosition
+                seekBarUpdateHandler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+    private fun startUpdatingSeekBar() {
+        seekBarUpdateHandler.post(seekBarUpdateRunnable)
+    }
+
+
     fun musicstart() {
         try {
-            if(music.isPlaying){
+            if (music.isPlaying) {
                 music.stop()
             }
             val currentAudio = audiofiles[currentIndex]
             val resId = resources.getIdentifier(currentAudio.toString(), "raw", packageName)
             music = MediaPlayer.create(this, resId)
             music.start()
+            startUpdatingSeekBar()
+            seekBar.max=music!!.duration
+            setdata()
         } catch (e: Exception) {
             Toast.makeText(this, "Error playing audio", Toast.LENGTH_SHORT).show()
         }
 
     }
+
+
     fun musicpause() {
-        if(music.isPlaying){
+        if (music.isPlaying) {
             music.pause()
-        }else{
+        } else {
             music.start()
+            seekBar.max=music!!.duration
         }
     }
 
@@ -67,4 +126,7 @@ class MainActivity : AppCompatActivity() {
         musicstart()
     }
 
+
 }
+
+
